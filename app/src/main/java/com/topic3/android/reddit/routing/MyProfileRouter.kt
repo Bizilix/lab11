2d9438ddc8e377b7ec95470ced856ef09c207c9a
Bizilix <gleb.moz@mail.ru>
2023-05-25 21:35:26 +0300

    MyProfileRouter.ktfile

--- app/src/main/java/com/topic3/android/reddit/routing/MyProfileRouter.kt
+++ app/src/main/java/com/topic3/android/reddit/routing/MyProfileRouter.kt
@@ -3,11 +3,7 @@ package com.topic3.android.reddit.routing
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 
-/**
- * Класс, определяющий экраны, которые есть в нашем приложении.
- *
- * Эти объекты должны соответствовать файлам, которые есть в пакете screens
- */
+
 sealed class MyProfileScreenType {
   object Posts : MyProfileScreenType()
   object About : MyProfileScreenType()
@@ -19,4 +15,5 @@ object MyProfileRouter {
   fun navigateTo(destination: MyProfileScreenType) {
     currentScreen.value = destination
   }
+}
 }
