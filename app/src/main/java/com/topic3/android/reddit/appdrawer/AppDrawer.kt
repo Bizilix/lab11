872ab99c059c90d981d0dd30360c1c831ae23465
Bizilix <gleb.moz@mail.ru>
2023-05-25 21:06:31 +0300

    AppDrawer.kt (AppDrawer)

--- app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
+++ app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
@@ -2,25 +2,28 @@ package com.topic3.android.reddit.appdrawer
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.topic3.android.reddit.R
-
 import com.topic3.android.reddit.theme.RedditThemeSettings
+
 
 /**
  * Представляет корневую композицию для панели приложений, используемой на экранах.
@@ -46,102 +49,3 @@ fun AppDrawer(
 /**
  * Представляет заголовок drawer приложения со значком и названием приложения.
  */
-@Composable
-private fun AppDrawerHeader() {
-  //TODO add your code here
-}
-
-@Composable
-fun ProfileInfo() {
-  //TODO add your code here
-}
-
-@Composable
-private fun ProfileInfoItem(
-  iconAsset: ImageVector,
-  amountResourceId: Int,
-  textResourceId: Int,
-  modifier: Modifier
-) {
-  //TODO add your code here
-}
-
-/**
- * Представляет действия drawer приложения:
- * * экранная навигация
- * * светлый/темный режим приложения
- */
-@Composable
-private fun AppDrawerBody(closeDrawerAction: () -> Unit) {
-  //TODO add your code here
-}
-
-/**
- * Представляет компонент в панели приложений, который пользователь может использовать для смены экрана.
- */
-@Composable
-private fun ScreenNavigationButton(
-  icon: ImageVector,
-  label: String,
-  onClickAction: () -> Unit,
-  modifier: Modifier = Modifier
-) {
-  val colors = MaterialTheme.colors
-
-  val surfaceModifier = modifier
-    .padding(start = 8.dp, top = 8.dp, end = 8.dp)
-    .fillMaxWidth()
-
-  Surface(
-    modifier = surfaceModifier,
-    color = colors.surface,
-    shape = MaterialTheme.shapes.small
-  ) {
-    TextButton(
-      onClick = onClickAction,
-      modifier = Modifier.fillMaxWidth()
-    ) {
-      Row(
-        horizontalArrangement = Arrangement.Start,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth()
-      ) {
-        Image(
-          imageVector = icon,
-          colorFilter = ColorFilter.tint(Color.Gray),
-          contentDescription = label
-        )
-        Spacer(Modifier.width(16.dp))
-        Text(
-          fontSize = 10.sp,
-          text = label,
-          style = MaterialTheme.typography.body2,
-          color = colors.primaryVariant
-        )
-      }
-    }
-  }
-}
-
-/**
- * Представляет компонент настройки в панели приложений.
- */
-@Composable
-private fun AppDrawerFooter(modifier: Modifier = Modifier) {
-  //TODO add your code here
-}
-
-private fun changeTheme() {
-  RedditThemeSettings.isInDarkTheme.value = RedditThemeSettings.isInDarkTheme.value.not()
-}
-
-@Preview
-@Composable
-private fun ProfileInfoItemPreview() {
-  ProfileInfoItem(
-    Icons.Filled.ShoppingCart,
-    R.string.default_reddit_age_amount,
-    R.string.reddit_age,
-    Modifier
-  )
-}
