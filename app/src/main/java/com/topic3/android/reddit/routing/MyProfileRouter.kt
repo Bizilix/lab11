6e44750c2944cae5cfa3002dd227b5107362018a
Bizilix <gleb.moz@mail.ru>
2023-05-25 21:36:59 +0300

    MyProfileRouter.kt

--- app/src/main/java/com/topic3/android/reddit/routing/MyProfileRouter.kt
+++ app/src/main/java/com/topic3/android/reddit/routing/MyProfileRouter.kt
@@ -2,18 +2,34 @@ package com.topic3.android.reddit.routing
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import com.topic3.android.reddit.R
 
 
-sealed class MyProfileScreenType {
-  object Posts : MyProfileScreenType()
-  object About : MyProfileScreenType()
+
+
+sealed class Screen(val titleResId: Int) {
+  object Home : Screen(R.string.home)
+  object Subscriptions : Screen(R.string.subreddits)
+  object NewPost : Screen(R.string.new_post)
+  object MyProfile : Screen(R.string.my_profile)
+  object ChooseCommunity : Screen(R.string.choose_community)
 }
 
-object MyProfileRouter {
-  var currentScreen: MutableState<MyProfileScreenType> = mutableStateOf(MyProfileScreenType.Posts)
+object RedditRouter {
+  var currentScreen: MutableState<Screen> = mutableStateOf(
+    Screen.Home
+  )
 
-  fun navigateTo(destination: MyProfileScreenType) {
+  private var previousScreen: MutableState<Screen> = mutableStateOf(
+    Screen.Home
+  )
+
+  fun navigateTo(destination: Screen) {
+    previousScreen.value = currentScreen.value
     currentScreen.value = destination
   }
-}
+
+  fun goBack() {
+    currentScreen.value = previousScreen.value
+  }
 }
